062709923ff6252568599d73e7bf1bdeed13b52d
Vitor Correa <vitorcorrea18@gmail.com>
2022-06-15 14:01:10 -0400

    feat: set grid layout for desktop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './styles/index.css';
 
 class App extends React.Component {
   render () {
     return (
+      <body className='body_container'>
+      <aside className='menu_aside'>
+        <div className='avatar'>avatar</div>
+        <div className='void'>void</div>
+        <div className='about'>about</div>
+        <div className='projects'>projects</div>
+        <div className='contact'>contact</div>
+      </aside>
+      <main className='main_page'>
       <h1>OLÁ MUNDO</h1>
+      </main>
+      </body>
     );
   }
 }
