115f31c21a9e9696ea7529f38519be0ec0d034ef
Vitor Correa <vitorcorrea18@gmail.com>
2022-06-15 16:36:38 -0400

    fix: changing Introduction for short Intro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import './styles/index.css';
+import NavLabel from './components/navLabel';
 
 class App extends React.Component {
   render () {
     return (
-      <body className='body_container'>
+      <main className='body_container'>
       <aside className='menu_aside'>
         <label className='close_menu'>X</label>
         <div className='avatar'>avatar</div>
-        <div className='void'>void</div>
-        <div className='introduction'>introduction</div>
-        <div className='about'>about</div>
-        <div className='projects'>projects</div>
-        <div className='contact'>contact</div>
+        <div className='void'></div>
+        <div className='intro'><NavLabel name='Intro' /></div>
+        <div className='about'><NavLabel name='About me' /></div>
+        <div className='projects'><NavLabel name='Projects' /></div>
+        <div className='contact'><NavLabel name='Contact' /></div>
       </aside>
-      <main className='main_page'>
+      <section className='main_page'>
       <h1>OLÁ MUNDO</h1>
+      </section>
       </main>
-      </body>
     );
   }
 }
