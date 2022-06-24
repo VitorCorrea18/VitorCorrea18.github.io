c426ba2c6cec18800e1aadb249a8bfaad78d46e4
Vitor Correa <vitorcorrea18@gmail.com>
2022-06-24 17:49:47 -0400

    feat: add download cv on contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import DownloadBtn from '../components/downloadBtn';
+import frontEndCv from '../documents/frontEndCv.pdf';
+import backEndCv from '../documents/backEndCv.pdf';
 import { linkedIn, github, gmail } from '../images/icons';
 import SocialImage from '../components/socialImage';
 
@@ -11,6 +14,10 @@ class Contact extends React.Component {
           <SocialImage link='https://www.linkedin.com/in/vitorcorrea18/' image={linkedIn} alt={'LinkedIn'}/>
           <SocialImage link='https://github.com/VitorCorrea18' image={github} alt={'Github'}/>
           <SocialImage link='mailto:vitorcorrea18@gmail.com' image={gmail} alt={'Gmail'}/>
+        </div>
+        <div className='contact_download_div'>
+          <DownloadBtn name={'Front End CV'} file={frontEndCv} />
+          <DownloadBtn name={'Back End CV'} file={backEndCv} />
         </div>
       </section>
     );
